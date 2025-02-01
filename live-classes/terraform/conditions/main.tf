5916f17ca64276dca678dc9cce53796cd93854e0
Akas Patranobis <akaspatranobis@yahoo.com>
2025-02-01 12:20:31 +0530

    up

--- live-classes/terraform/conditions/main.tf
+++ live-classes/terraform/conditions/main.tf
@@ -2,8 +2,14 @@
 
 
 resource "aws_instance" "test" {
+    count=2
     ami = "ami-0533f2ba8a1995cf9"
     instance_type = var.instance_type == "t2*" ? "t3.micro" :  var.instance_type
+    tags = {
+        Name = "Server - ${count.index}"
+  }
 }
 
 variable "instance_type" {}
+
+# variable "create_instance" {}
