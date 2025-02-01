09d3a8710695cbdc9422c9a0d8cdfd51c4e767e1
Akas Patranobis <akaspatranobis@yahoo.com>
2025-02-01 12:06:26 +0530

    up

---
+++ live-classes/terraform/conditions/main.tf
@@ -0,0 +1,9 @@
+# ? and :
+
+
+resource "aws_instance" "test" {
+    ami = "ami-0533f2ba8a1995cf9"
+    instance_type = var.instance_type == "t2*" ? "t3.micro" :  var.instance_type
+}
+
+variable "instance_type" {}
